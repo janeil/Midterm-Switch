13ffe26d01666cf6ba7a441ec95e427e308a4caf
janeil <janeil>
2019-02-04 15:33:15 +0800

    added files

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>Midterm Switch</title>
+    </head>
+    
+    <script>
+            SelectElement("color", valueToSelect)
+            function colorswitch(id, valueToSelect) {
+                var color = document.getElementById(color);
+                element.value = valueToSelect;
+                document.body.style.backgroundColor = color;
+            }
+            
+            
+        </script>
+
+    <body>
+        <select onclick="colorswitch()" id="color">
+            <option value="red">Red</option>
+            <option value="blue">Blue</option>
+            <option value="green">Green</option>
+        </select>
+        
+    
+    
+    </body>
+</html>
